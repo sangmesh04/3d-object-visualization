cf3d46bcec0d2e1329b581aa78548b7f01545eef
sangmesh04 <78370367+sangmesh04@users.noreply.github.com>
2024-01-20 23:10:34 +0530

    adding and responsing the category list

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,19 +1,65 @@
 const Category = require("../models/category");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs-extra");
+
+let upload = multer({
+  storage: multer.diskStorage({
+    destination: async (req, file, cb) => {
+      // ** code for making directory using job ID make sure to change schema of file.js
+      let path = `./uploads/category`;
+      if (!fs.existsSync(path)) {
+        fs.mkdirSync(path, { recursive: true });
+      }
+      cb(null, path);
+    },
+    filename: async (req, file, cb) => {
+      //  ** with student auth Code
+      const uniqueName =
+        file.originalname.split(".")[0].split(" ")[0] +
+        "-" +
+        Math.floor(Math.random() * 1000000 + 1000000);
+      let filename = uniqueName;
+      req.filename = filename + path.extname(file.originalname);
+      cb(null, filename + path.extname(file.originalname));
+    },
+  }),
+}).single("image");
 
 module.exports.addCategory = async (req, res) => {
-  try {
-    const catData = req.body;
-    const category = await Category.create({ ...catData });
-    if (category) {
-      res
-        .status(200)
-        .json({ status: true, message: "Category added successfully!" });
-    } else {
-      res
-        .status(400)
-        .json({ status: false, message: "Unable to add category!" });
+  upload(req, res, async () => {
+    try {
+      const category = await Category.create({
+        name: req.body.name,
+        description: req.body.description,
+        image: `./uploads/category/${req.filename}`,
+      });
+      // console.log(resume);
+      if (category) {
+        res.status(201).json({
+          success: true,
+          message: "Category added successfully!",
+        });
+      } else {
+        res
+          .status(400)
+          .json({ success: false, message: "Unable to add category!" });
+      }
+    } catch (err) {
+      res.status(400).json({ success: false, message: err.message });
+      // ** code for resume-upload using student authentication middleware
+      if (fs.existsSync(`./uploads/category/${req.filename}`)) {
+        fs.unlink(`./uploads/category/${req.filename}`);
+      }
     }
+  });
+};
+
+module.exports.getCategory = async (req, res) => {
+  try {
+    const category = await Category.find();
+    res.status(200).json({ status: true, data: category });
   } catch (err) {
-    res.status(400).json({ status: false, error: err });
+    res.status(400).json({ status: false, message: err });
   }
 };
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -5,4 +5,6 @@ const router = express.Router();
 
 router.post("/category/add", authAdmin, categoryController.addCategory);
 
+router.get("/category", authAdmin, categoryController.getCategory);
+
 module.exports = router;
